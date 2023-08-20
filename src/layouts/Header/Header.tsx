8e707f27fa7ce05e447be836bb0587621b7059aa
curlyjun <tjdwns4850@gmail.com>
2023-08-20 15:38:54 +0900

    <feat> 임시 헤더 추가

---
+++ src/layouts/Header/Header.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import * as S from "./Header.styled";
+import { HeaderProps } from "./Header.types";
+
+const Header = () => {
+  return <S.Header>헤더임다</S.Header>;
+};
+
+export default Header;
