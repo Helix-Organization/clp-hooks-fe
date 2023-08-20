56c5e7fa10519e0c2ecda0f6e801dafb9493e084
curlyjun <tjdwns4850@gmail.com>
2023-08-20 16:14:53 +0900

    <feat> add Primary color

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,6 +1,24 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
 import { reset } from "./reset";
+
+export const variables = css`
+  :root {
+    --primary-color: #0066ff;
+  }
+`;
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
+
+  
+  body {
+    font-size: 16px;
+  }
+  
+
+  * {
+    box-sizing: border-box;
+  }
+
+  ${variables}
 `;
