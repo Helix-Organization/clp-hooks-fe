1179c7209040b5df996c40bc44428a8dc06f417a
curlyjun <tjdwns4850@gmail.com>
2023-08-20 16:18:01 +0900

    <feat> 헤더에 임시 로고 추가 및 지갑 연결 버튼 추가

--- src/layouts/Header/Header.styled.ts
+++ src/layouts/Header/Header.styled.ts
@@ -1,5 +1,24 @@
 import styled from "styled-components";
 
 export const Header = styled.header`
-  background-color: red;
+  display: flex;
+  flex-flow: row;
+  width: 100%;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  position: fixed;
+  top: 0px;
+  z-index: 1000;
 `;
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  width: 100%;
+  height: 72px;
+  z-index: 2;
+`;
+
+export const IconWrapper = styled.div``;
--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,10 +1,18 @@
 "use client";
 
+import { WalletConnect } from "@/components/WalletConnect";
 import * as S from "./Header.styled";
 import { HeaderProps } from "./Header.types";
 
 const Header = () => {
-  return <S.Header>헤더임다</S.Header>;
+  return (
+    <S.Header>
+      <S.Nav>
+        <S.IconWrapper>🖤</S.IconWrapper>
+        <WalletConnect />
+      </S.Nav>
+    </S.Header>
+  );
 };
 
 export default Header;
